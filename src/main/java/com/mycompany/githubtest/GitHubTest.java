16a609098c97380ba2a06432d360d3717ec04439
ella-BR <153469544+ella-BR@users.noreply.github.com>
2023-12-12 11:08:45 -0500

    Whatever

---
+++ src/main/java/com/mycompany/githubtest/GitHubTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.githubtest;
+
+/**
+ *
+ * @author ElRim1609
+ */
+public class GitHubTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
